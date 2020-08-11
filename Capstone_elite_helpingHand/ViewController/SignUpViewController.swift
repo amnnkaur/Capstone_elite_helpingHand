62040082b2f13a1dd6c9a013aaafc15ded525cf5
Aman Kaur <amankaur2628@gmail.com>
2020-08-11 14:23:14 -0400

    [Aman] State and City Picker View value updated

--- Capstone_elite_helpingHand/ViewController/SignUpViewController.swift
+++ Capstone_elite_helpingHand/ViewController/SignUpViewController.swift
@@ -12,15 +12,51 @@ class SignUpViewController: UIViewController,UIPickerViewDelegate, UIPickerViewD
   
  
     @IBOutlet weak var city: UITextField!
+    @IBOutlet weak var state: UITextField!
+    var cityPicker = UIPickerView()
+    var statePicker = UIPickerView()
+    private var _currentSelection: Int = 0
+    
+    let stateArray = ["Ontario", "Alberta", "British Columbia", "Manitoba"]
     
     
-    let countryArray = ["Brampton", "Toronto", "Mississauga", "Etobicoke", "Scarbourough", "Vaughan", "Windsor"]
+    let cityArrayON = [["Brampton", "Toronto", "Mississauga", "Ottawa", "Hamilton", "Kitchener", "Vaughan", "Windsor", "Markham", "London"],
+                       ["Calgary", "Edmonton", "Lethbridge", "Brooks"],
+                       ["Abbotsford", "Burnaby", "Vancouver", "Surrey","Richmond", "Kimberley" ,"Duncan"],
+                       ["Brandon", "Winnipeg", "Thompson", "Winkler"]]
+    
+    var currentSelection: Int {
+        get {
+            return _currentSelection
+        }
+        set {
+            _currentSelection = newValue
+            cityPicker .reloadAllComponents()
+            statePicker .reloadAllComponents()
+
+            state.text = stateArray[_currentSelection]
+            city.text = cityArrayON[_currentSelection][0]
+
+        
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let cityPicker = UIPickerView()
-        city.inputView = cityPicker
+        
+        currentSelection = 0;
+
+        statePicker.delegate = self
+        statePicker.dataSource = self
+        statePicker.tag = 0
+        
         cityPicker.delegate = self
+        cityPicker.dataSource = self
+        cityPicker.tag = 1
+        
+        state.inputView = statePicker
+        city.inputView = cityPicker
+        
     
     }
     
@@ -29,25 +65,33 @@ class SignUpViewController: UIViewController,UIPickerViewDelegate, UIPickerViewD
       }
       
       func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        countryArray.count
+         if pickerView.tag == 0 {
+               return stateArray.count
+           } else {
+               return cityArrayON[currentSelection].count
+           }
       }
     
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-       let row = countryArray[row]
-       return row
+       if pickerView.tag == 0 {
+           return stateArray[row]
+       } else {
+           return cityArrayON[currentSelection][row]
+       }
     }
     
     func pickerView( _ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        city.text = countryArray[row]
-    }
-    /*
-    // MARK: - Navigation
+        if pickerView.tag == 0 {
+                currentSelection = row
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+               state.text = stateArray[row]
+                state.resignFirstResponder()
+            
+            } else if pickerView.tag == 1 {
+                city.text = cityArrayON[currentSelection][row]
+                city.resignFirstResponder()
+            }
+        
     }
-    */
 
 }
