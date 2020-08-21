b35055ec8190bd4a9e8ceca02f06feb475d69550
Anmol Singh <anmolsingh1597@gmail.com>
2020-08-21 10:33:18 -0400

    [Anmol Singh]User get logs of various tasks posted by him and its specified replies to avoid confusion

--- Capstone_elite_helpingHand/ViewController/MyTaskRepliesTableViewController.swift
+++ Capstone_elite_helpingHand/ViewController/MyTaskRepliesTableViewController.swift
@@ -1,38 +1,43 @@
 //
-//  MyTaskTableViewController.swift
+//  MyTaskRepliesTableViewController.swift
 //  Capstone_elite_helpingHand
 //
-//  Created by Aman Kaur on 2020-08-20.
+//  Created by Anmol singh on 2020-08-21.
 //  Copyright © 2020 Aman Kaur. All rights reserved.
 //
 
 import UIKit
-//import FirebaseAuth
 
-class MyTaskTableViewController: UITableViewController {
+class MyTaskRepliesTableViewController: UITableViewController {
     
-    var userTasks: [Task] = []
-    @IBOutlet weak var postedBy: UILabel!
-    @IBOutlet weak var jobTitle: UILabel!
-    @IBOutlet weak var jobDesc: UILabel!
-    @IBOutlet weak var postedDate: UILabel!
-    
+    var task: Task?
+    var repliesMessageList: [CustomerMessages] = []
+    var filteredList: [CustomerMessages] = []
+    var userList: [User] = []
     override func viewDidLoad() {
         super.viewDidLoad()
-        userTasks = DataStorage.getInstance().getAllUserTask()
+
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        initials()
+        
+        
+    }
+    
+    func initials() {
+        userList = DataStorage.getInstance().getAllUsers()
+        repliesMessageList = DataStorage.getInstance().getAllMessages()
+        for item in repliesMessageList {
+            if (item.taskEmail == task?.taskEmail && item.taskTitle == task?.taskTitle && item.taskPostingDate == task?.taskDueDate){
+                self.filteredList.append(item)
+            }
+        }
     }
 
     // MARK: - Table view data source
-    
-    override func tableView(_ tableView: UITableView, titleForHeaderInSection
-                                   section: Int) -> String? {
-          return "Tasks posted by you:"
-       }
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
@@ -41,46 +46,43 @@ class MyTaskTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return self.userTasks.count
+        return filteredList.count
     }
 
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection
+                                      section: Int) -> String? {
+        return "Replies for: \(task?.taskTitle ?? "No such task name is there")"
+          }
+    
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-       let cell = tableView.dequeueReusableCell(withIdentifier: "myTaskCell") as! TaskTableViewCell
-        
-        let task = self.userTasks[indexPath.row]
-        
-        let backgroundColorView = UIView()
-        backgroundColorView.backgroundColor = UIColor.clear
-        cell.selectedBackgroundView = backgroundColorView
-//        cell.layer.shadowOffset = CGSize(width: 0, height: 2)
-//        cell.layer.shadowColor = UIColor.black.cgColor
-//        cell.layer.shadowRadius = 5
+        let cell = tableView.dequeueReusableCell(withIdentifier: "repliesCell", for: indexPath)
 
-        cell.layer.cornerRadius = 20
-//        cell.layer.shadowOpacity = 0.40
-        cell.layer.masksToBounds = true;
-        cell.clipsToBounds = false;
-        
-        cell.postedBy.text = task.taskEmail
-        cell.jobTitle.text = task.taskTitle
-        cell.jobDesc.text = task.taskDesc
-        cell.postedDate.text = task.taskDueDate
-        
+        let replies = filteredList[indexPath.row]
+        // Configure the cell...
+        for item in self.userList {
+            if replies.userEmail  == item.emailId {
+                cell.textLabel?.text = "\(item.firstName.capitalizingFirstLetter()) \(item.lastName.capitalizingFirstLetter())"
+                cell.detailTextLabel?.text = item.emailId
+            }
+        }
+     
         return cell
     }
    
-
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
-        let task = userTasks[indexPath.row]
-        
-        if let viewController = storyboard?.instantiateViewController(identifier: "myTasksRepliesVC") as? MyTaskRepliesTableViewController{
-            viewController.task = task
-            present(viewController, animated: true, completion: nil)
-        }
+         let replies = self.filteredList[indexPath.row]
+               
+               if let viewController = storyboard?.instantiateViewController(identifier: "ChatVC") as? ChatViewController {
+                 
+                    viewController.user2Name = replies.taskTitle + " || " + replies.taskEmail
+                    viewController.user2UID = replies.userUID
+                   
+                   
+                   present(viewController, animated: true, completion: nil)
+               }
     }
-    
+
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
